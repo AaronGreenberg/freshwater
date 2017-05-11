c0673f3c2b4fe4bd1020d53990d089feeb069bd6
daft <daft@crabwork.smile.org>
2017-05-10 17:09:19 -0700

    commiting

--- growth5/growth_tab5.R
+++ growth5/growth_tab5.R
@@ -1,9 +1,13 @@
+library(maps)
+library(mapdata)
+
+main <- function()
+{            
 haspclist<-read.csv("RB_haspc2017_master_list.csv")
 assessed<-scan('assessed_wbid.csv',what="character",skip=1)
 lakepars<-read.csv('model_17_lakepars.csv',header=T)
 mainpars<-read.csv('model_17_mainpars.csv',header=T)
-library(maps)
-library(mapdata)
+
 gnpars<-match(haspclist$WATERBODY_IDENTIFIER,assessed)
 vK=vLinf=vector(length=nrow(haspclist))
 for(i in 1:length(gnpars))
@@ -65,8 +69,15 @@ s2=s1/(target-(L0[2]*exp(-vKye*dt_ye)))
 s3=s2-1
 s4=s3/beta_ye
 s5=s4+0.2
-s6_ye=(s5*10^5)/L0[2]^2
+    s6_ye=(s5*10^5)/L0[2]^2
+out <- list(s6_ye=s6_ye,s6_fry=s6_fry)
+}
 
+
+
+plots6fry <- function(inp)
+{                 
+haspclist<-read.csv("RB_haspc2017_master_list.csv")
 lonlim<-c(-130,-115)#range(c(obj@lakex,obj@pcx))+c(0.5,-0.5)
 latlim<-c(48.4,56.5)#range(c(obj@lakey,obj@pcy))+c(-0.5,0.5)
 # lcol<-rep("#99999970",obj@nl)
@@ -75,20 +86,45 @@ latlim<-c(48.4,56.5)#range(c(obj@lakey,obj@pcy))+c(-0.5,0.5)
 lakepoints<-data.frame(X=haspclist$LONGITUDE,Y=haspclist$LATITUDE)
 EID=seq(1:nrow(haspclist))
 
-jpeg("img_fry.jpg",width=9,height=6,units="in",res=600)
+
+#jpeg("img_fry.jpg",width=9,height=6,units="in",res=600)
 plot(lonlim,latlim,col="white",axes=F,xlab="",ylab="")
 map(database = "worldHires", xlim=lonlim, ylim=latlim,resolution = 0,fill=T,col='white',mar=rep(0.3,4),add=T,lwd=1)#make a first plot of the mapto define the range of the plot 
-cols<-ifelse(s6_fry<=0,"grey",ifelse(s6_fry<100,"red",ifelse(s6_fry<200,"orange",ifelse(s6_fry<500,"green","blue"))))
+cols<-ifelse(inp$s6_fry<=0,"grey",ifelse(inp$s6_fry<100,"red",ifelse(inp$s6_fry<200,"orange",ifelse(inp$s6_fry<500,"green","blue"))))
 points(haspclist$LONGITUDE,haspclist$LATITUDE,pch=1,cex=1,col=cols)
-dev.off()
+#dev.off()
+}
 
-jpeg("img_ye.jpg",width=9,height=6,units="in",res=600)
+plots6ye <- function(inp)
+{
+haspclist<-read.csv("RB_haspc2017_master_list.csv")    
+lonlim<-c(-130,-115)#range(c(obj@lakex,obj@pcx))+c(0.5,-0.5)
+latlim<-c(48.4,56.5)#range(c(obj@lakey,obj@pcy))+c(-0.5,0.5)
+# lcol<-rep("#99999970",obj@nl)
+# llcol=rep('#99999970',obj@nl)
+
+lakepoints<-data.frame(X=haspclist$LONGITUDE,Y=haspclist$LATITUDE)
+EID=seq(1:nrow(haspclist))
+
+
+#jpeg("img_ye.jpg",width=9,height=6,units="in",res=600)
 plot(lonlim,latlim,col="white",axes=F,xlab="",ylab="")
 map(database = "worldHires", xlim=lonlim, ylim=latlim,resolution = 0,fill=T,col='white',mar=rep(0.3,4),add=T,lwd=1)#make a first plot of the mapto define the range of the plot 
-cols<-ifelse(s6_ye<=0,"grey",ifelse(s6_ye<100,"red",ifelse(s6_ye<200,"orange",ifelse(s6_ye<500,"green","blue"))))
+cols<-ifelse(inp$s6_ye<=0,"grey",ifelse(inp$s6_ye<100,"red",ifelse(inp$s6_ye<200,"orange",ifelse(s6_ye<500,"green","blue"))))
 points(haspclist$LONGITUDE,haspclist$LATITUDE,pch=1,cex=1,col=cols)
-dev.off()
 
-s1=vLinf*exp(ppt*(map/1000-0.5796408))*(1-exp(-vKye*dt_ye))
-s2=s1/(target-(10*exp(-vK*dt_ye)))
-s6=(s5*10^5)/10^2
+}
+
+
+s <- main()
+plots6ye(s)
+x11()
+plots6fry(s)
+
+
+
+#dev.off()
+
+## s1=vLinf*exp(ppt*(map/1000-0.5796408))*(1-exp(-vKye*dt_ye))
+## s2=s1/(target-(10*exp(-vK*dt_ye)))
+## s6=(s5*10^5)/10^2
