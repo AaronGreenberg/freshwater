b9914721863103d41a583def6d4bb85a476ac847
daft <daft@crabwork.smile.org>
2017-05-19 19:19:42 -0700

    committing with inverse

--- growth1/growth_tab1.R
+++ growth1/growth_tab1.R
@@ -112,7 +112,132 @@ colnames(L_hat)<-paste0("age_",age)
     
 }
 
-fig1 <- function(L_hat)
+
+
+
+main2 <- function(wbid,lwts,kf,sden,strain,ploidy,assessed,in_hatch,masterlist,mainpars_post,lakepars_post)
+{            
+#read lakes list
+
+#parameters
+
+bet_post<-new('list');ple_post<-new('list');stn_post<-new('list')
+bet_post[[1]]<-mainpars_post$bet1
+bet_post[[2]]<-mainpars_post$bet2
+osp_post<-mainpars_post$osp
+ple_post[[1]]<-mainpars_post$ple1
+ple_post[[2]]<-mainpars_post$ple2
+ple_post[[3]]<-0
+ppt_post <-	mainpars_post$
+
+stn_post[[1]]<-mainpars_post$stn1
+stn_post[[2]]<-mainpars_post$stn2
+stn_post[[3]]<-mainpars_post$stn3
+stn_post[[4]]<-mainpars_post$stn4
+stn_post[[5]]<-mainpars_post$stn5
+stn_post[[6]]<-0
+yef_post<-mainpars_post$yef
+
+
+
+i1=which(masterlist$WATERBODY_IDENTIFIER==wbid)
+lake_area<-masterlist$AREA_HA[i1]
+
+i2=any(assessed==wbid)
+if(i2==TRUE)
+{i3=which(assessed==wbid);K<-lakepars_post[,i3];Linf<-lakepars_post[,(91+i3)]} else{K<-mainpars_post[,1];Linf<-mainpars_post[,2]}
+
+
+
+L0=round(((lwts/(kf/100000))^(1/3)),0)/10 #converting weight in g to length in cm
+
+lwd=as.character(discretize(lwts,"fixed",categories = in_hatch$cat,labels=in_hatch$labs))
+ind=which(in_hatch$labs==lwd) #adding error around stocking size
+cv=ifelse(length(ind)==1,in_hatch$cv[ind],0.1)
+sig=cv*L0
+tau=1/(sig*sig)
+lfs=ifelse(lwts<=4,1,2)
+fy=lfs-1
+L0_vec<-rnorm(length(Linf),mean=L0,sd=sig)
+
+
+#sden<-snum/lake_area #stocking density - total numbers/area
+dens0<-(sden*L0^2)/10^5 #density expressed in NL2
+drb<-ifelse(lfs==1,dens0-0.0375,dens0-0.2)
+dos<-0
+
+#Precipation
+map<-masterlist$MAP[i1]/1000-0.5796408
+
+#delta_t calculation #no inputs from gui
+if(lfs==1) rda="2010/10/15" else rda="2011/06/01"
+eda<-c("2012/6/15","2013/6/15","2014/6/15","2015/6/15","2016/6/15")
+dt=vector(length=length(eda))
+for(i in 1:length(eda))
+{
+  x1=seq.Date(as.Date(rda),as.Date(eda[i]),by='day')
+  y1=month(x1)
+  mm=mm2=vector(mode="numeric",length=12)
+  for(j in 1:12)
+  {
+    mm[j]=ifelse(length(which(y1==j))<1,0,length(which(y1==j)))
+    mm2[j]=ifelse(length(i1)<1,NA,mm[j]/30*masterlist[i1,9+j])
+  }
+  dt[i]=sum(mm2)/1000
+
+}
+
+Linf_L <- (Linf*exp(ppt_post*map))/(1+bet_post[[lfs]]*drb+osp_post*dos)
+K_Lp <- K*exp(yef_post*fy+stn_post[[strain]]+ple_post[[ploidy]])
+L_hat<-matrix(data=0,nrow=length(Linf),ncol=length(eda))
+for(i in 1:length(eda))
+{
+  L_hat[,i] <- L0_vec*exp(-K_Lp*dt[i])+Linf_L*(1-exp(-K_Lp*dt[i]))
+}
+age=seq(2,6)
+colnames(L_hat)<-paste0("age_",age)
+                                        #figure output
+    out=list(L_hat=L_hat)
+    
+}
+
+
+
+main3 <-  function(wbid,lwts,kf,target,strain,ploidy,age)
+{
+    assessed<-read.csv('assessed_wbid.csv',colClasses=c("character"))
+    in_hatch=read.csv('inhatchery_results.csv')
+    masterlist<-read.csv("RB_tool_master_list.csv")
+    mainpars_post<-read.csv('model_17post_mainpars.csv')
+    lakepars_post<-read.csv('model_17post_lakepars.csv')
+age=age-1 
+    
+    f <- function(sden,age){mean(main2(wbid,lwts,kf,sden,strain,ploidy,assessed,in_hatch,masterlist,mainpars_post,lakepars_post)$L_hat[,age])-target}
+  z <- tryCatch(
+        {
+            
+            uniroot(f,c(0,10000),age)
+        },
+        error=function(cond) {
+            message(paste("I have a bad feeling about this. \n"))
+            message(paste("Are you sure that you entered a plausible target size?! \n"))
+            message(paste("I cannot find a stocking density that works!. \n"))
+            message("Here's the original error message:")
+            message(paste(cond,"\n"))
+            message("Sorry:\n")
+            # Choose a return value in case of error
+            root=10
+            return(list(root=root))
+        }
+)
+   
+    
+
+return(z)
+}
+
+
+fig1 <- function(L_hat,density,target)
 {
 
     means=round(apply(L_hat,2,mean),1)
@@ -121,6 +246,11 @@ fig1 <- function(L_hat)
    max(means+5*sds)))
     text(1:6, means+5*sds, paste("mu=",means))
     text(1:6, means-5*sds, paste("sd=",sds))
+    if(!missing(target))
+    {
+        abline(h=target,col="red")
+    }
+    legend("topleft",legend=paste("Stocking Density:=",signif(density,4)))
 #age should be 2 to 6
 }
 
@@ -146,5 +276,20 @@ tab1 <- function(L_hat)
                   
 #Please include summaries shown on tab of the tool                  
 s=main(wbid,lwts,kf,sden,strain,ploidy)
-fig1(s$L_hat)
+
+fig1(s$L_hat,sden)
 tab1(s$L_hat)
+s2=main3(wbid,lwts,kf,14,strain,ploidy,3)
+s=main(wbid,lwts,kf,s2$root,strain,ploidy)
+tab1(s$L_hat)
+
+s2=main3(wbid,lwts,kf,10,strain,ploidy,2)
+s=main(wbid,lwts,kf,s2$root,strain,ploidy)
+tab1(s$L_hat)
+fig1(s$L_hat,s2$root,10)
+
+
+s2=main3(wbid,lwts,kf,13,strain,ploidy,4)
+s=main(wbid,lwts,kf,s2$root,strain,ploidy)
+tab1(s$L_hat)
+fig1(s$L_hat,s2$root,13)
