c52cadd975c671620d7e5e1743097ee5d126d30e
daft <aaron@gmail.com>
2017-12-07 13:21:18 -0800

    committing bcfreshwater growth 4

---
+++ growth4/server.R
@@ -0,0 +1,30 @@
+library(shiny)
+shinyServer(function(input, output){
+
+source("growth_tab4.R")
+print("Hi")
+kf <- 1.1
+dataInput <- reactive({
+    mainout <-main(input$sden,input$eta)
+out=list(mainout=mainout)
+return(out)
+})
+
+
+output$mainPlot1 <- renderPlot({
+    out <- dataInput()
+    print("silly")
+   plotfry(out$mainout,as.numeric(input$straintype))
+}
+,height = 600,width=600)
+
+output$mainPlot2 <- renderPlot({
+    out <- dataInput()
+    print("silly")
+    plotye(out$mainout,as.numeric(input$straintype))
+}
+,height = 600,width=600)
+
+})
+
+
---
+++ growth4/ui.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(theme=shinytheme("flatly"),
+  # Application title
+  titlePanel("Size Distribution"),
+#For strains: Blackwater=1,Carp=2,Gerrard=3,Multiple=4,Pennask=5
+  # Sidebar with a slider input for the number of bins
+#sden<-c(20,50,100,200,500)
+  sidebarLayout(
+      sidebarPanel(
+          radioButtons("straintype", "Strain:",
+             c(
+                 "Blackwater" = 1,
+                  "Carp" = 2,
+                  "Fraser Valley"=3,
+                  "Gerrard"=4,
+                  "Multiple"=5,
+                  "Pennask"=6
+),inline=TRUE),
+          radioButtons("sden", "Stocking Density:",
+             c("20" = 20,
+               "50" = 50,
+                "100"=100,
+                 "200"=200,
+                 "500"=500
+            ),inline=TRUE),
+
+dateInput("eta", "eta", value = NULL, min = NULL, max = NULL,
+  format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+  language = "en", width = NULL)),
+
+    # Show a plot of the generated distribution
+      mainPanel(
+          tabsetPanel(type = "tabs",
+                      tabPanel("Map Fry ", plotOutput("mainPlot1",width="100%")),
+                      tabPanel("Map Yearling ", plotOutput("mainPlot2",width="100%"))
+
+)))))
+
